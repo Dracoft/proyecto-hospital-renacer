efca07a733d5800ac82b24647b88b8aa66c162a9
Incógnita X <julian.cortes43@gmail.com>
2023-10-08 22:46:53 -0500

    erres

--- app.mjs
+++ app.mjs
@@ -1,4 +1,4 @@
-var createError = require('http-errors');
+import createError from 'http-errors';
 var express = require('express');
 const session = require('express-session');
 var path = require('path');
